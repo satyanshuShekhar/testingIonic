ed9659c896a1dfd492ac22016fa3d586ee3d8320
satyanshu <satyanshu46@gmail.com>
2020-03-16 00:04:59 +0530

    Predefined home page deleted, added two new page login and home. UI changes for login page and also added routing.

---
+++ src/app/Pages/login/login.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+//import router for navigation
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  //When we define public router: Router, Angular will create an object for Router class.
+  constructor(public router: Router) { }
+
+  ngOnInit() {
+  }
+
+  //Login Method for Routing to home from login
+  login() {
+    this.router.navigate(['/home']);
+  }
+
+}
