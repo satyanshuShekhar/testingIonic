43087ea3ea5a2c9d02f5ca4a3e9a4ef81bcf889d
satyanshu <satyanshu46@gmail.com>
2020-03-21 23:48:53 +0530

    changed starter path.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'starter', pathMatch: 'full' },
   {
     path: 'login',
     loadChildren: () => import('./Pages/login/login.module').then( m => m.LoginPageModule)
