c523a8ff54cdb25f4f68eeedc8b4ddcb808ef8e8
Tanushripandey <tanushri.it@gmail.com>
2020-03-28 15:28:17 +0530

    .

--- src/app/Pages/forgot-password/forgot-password.page.ts
+++ src/app/Pages/forgot-password/forgot-password.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import {ReactiveFormsModule, FormGroup , FormControl , Validators, FormBuilder}from '@angular/forms' ;
+import { NavController, NavParams } from '@ionic/angular';
+
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
 })
-export class ForgotPasswordPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+export class ForgotPasswordPage  {
+  get email(){
+    return this.ForgotForm.get("email");
   }
+ 
+  ForgotForm =this.formBuilder.group({
+    email:[
+      "",  
+  [
+    Validators.required,
+    Validators.pattern("^[a-zA-Z0-9.%-]+@[a-zA-Z0-9.-]+.[a-zA-z]{2-4}$")
+  ]
+]
+  });
 
+    
+
+  constructor(private formBuilder:FormBuilder)
+   {
+
+ }
+ 
+  
 }
+
+
