e18cca1917d28e0f5155c14e86b6f2bfa1f3a4da
Tanushripandey <tanushri.it@gmail.com>
2020-04-17 18:26:52 +0530

    mock data

--- src/app/Pages/home/home.page.ts
+++ src/app/Pages/home/home.page.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { imageData } from '../../../assets/Models/imagedata';
+import imagedata from '../../../assets/json/imagedata.json' ;
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+  data:  imageData[] = [];
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.data = [...imagedata.imagedata];
+    console.log(this.data);
   }
 
   cardDetail() {
---
+++ src/assets/Models/imagedata.ts
@@ -0,0 +1,10 @@
+export interface imageData {
+    id?: number;
+    FullTeamName?: string;
+    ShortName?: string;
+    img?: any;
+}
+
+export interface EachimageDetails {
+    images?: imageData[];
+}
